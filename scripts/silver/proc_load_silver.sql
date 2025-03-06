3cabddad20ecd7e72b0b0143fa8ba2509b3dbed3
TheNisham <77533996+nishamghimire5@users.noreply.github.com>
2025-03-06 22:23:38 +0545

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -3,21 +3,22 @@
 Stored Procedure: Load Silver Layer (Bronze -> Silver)
 ===============================================================================
 Script Purpose:
-    This stored procedure loads data into the 'silver' schema from the 'bronze' schema.
+    This stored procedure loads data into the 'silver' schema from the 'bronze' layer. 
     It performs the following actions:
-    - Transforms and cleans data from bronze tables.
-    - Inserts the transformed data into the corresponding silver tables.
-    - Handles data type conversions and ensures data consistency.
+    - Truncates the silver tables before loading data.
+    - Cleanses and transforms data from bronze tables.
+    - Handles null values, standardizes formats, and applies business logic.
+    - Inserts the transformed data into corresponding silver tables.
+
 Parameters:
     None. 
     This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
-    EXEC silver.proc_load_silver;
+    EXEC silver.load_silver;
 ===============================================================================
 */
-
-CREATE OR ALTER PROCEDURE silver.proc_load_silver
+CREATE OR ALTER PROCEDURE silver.load_silver
 AS
 BEGIN
     DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
@@ -28,14 +29,37 @@ BEGIN
         PRINT('==================================================');
 
         PRINT('--------------------------------------------------');
-        PRINT('Loading CRM Tables...');
+        PRINT('Truncating Silver Tables...');
         PRINT('--------------------------------------------------');
 
-        -- Transform and load customer information into silver.crm_cust_info
-        SET @start_time = GETDATE();
-        PRINT('Transforming and loading data into silver.crm_cust_info');
+        -- Truncate silver tables before loading
+        PRINT('Truncating Table: silver.crm_cust_info');
+        TRUNCATE TABLE silver.crm_cust_info;
 
-        WITH LatestRecords
+        PRINT('Truncating Table: silver.crm_prd_info');
+        TRUNCATE TABLE silver.crm_prd_info;
+
+        PRINT('Truncating Table: silver.crm_sales_details');
+        TRUNCATE TABLE silver.crm_sales_details;
+
+        PRINT('Truncating Table: silver.erp_cust_az12');
+        TRUNCATE TABLE silver.erp_cust_az12;
+
+        PRINT('Truncating Table: silver.erp_loc_a101');
+        TRUNCATE TABLE silver.erp_loc_a101;
+
+        PRINT('Truncating Table: silver.erp_px_cat_g1v2');
+        TRUNCATE TABLE silver.erp_px_cat_g1v2;
+
+        PRINT('--------------------------------------------------');
+        PRINT('Transforming and Loading CRM Tables...');
+        PRINT('--------------------------------------------------');
+
+        -- Transform and Load crm_cust_info
+        SET @start_time = GETDATE();
+        PRINT('Transforming Data for Table: silver.crm_cust_info');
+        WITH
+        LatestRecords
         AS
         (
             SELECT
@@ -57,28 +81,28 @@ BEGIN
                 ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_Last
             FROM bronze.crm_cust_info
         )
-        INSERT INTO silver.crm_cust_info
-            (cst_id, cst_key, cst_firstname, cst_lastname, cst_marital_status, cst_gender, cst_create_date)
-        SELECT
-            cst_id,
-            cst_key,
-            cst_firstname,
-            cst_lastname,
-            cst_marital_status,
-            cst_gender,
-            cst_create_date
-        FROM LatestRecords
-        WHERE flag_Last = 1 AND cst_id IS NOT NULL;
+    INSERT INTO silver.crm_cust_info
+        (cst_id, cst_key, cst_firstname, cst_lastname, cst_marital_status, cst_gender, cst_create_date)
+    SELECT
+        cst_id,
+        cst_key,
+        cst_firstname,
+        cst_lastname,
+        cst_marital_status,
+        cst_gender,
+        cst_create_date
+    FROM LatestRecords
+    WHERE flag_Last = 1 AND cst_id IS NOT NULL;
 
         SET @end_time = GETDATE();
         PRINT('Time taken to load silver.crm_cust_info: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds');
         PRINT('----------------------------------------------');
 
-        -- Transform and load product information into silver.crm_prd_info
+        -- Transform and Load crm_prd_info
         SET @start_time = GETDATE();
-        PRINT('Transforming and loading data into silver.crm_prd_info');
-
-        WITH TransformedData
+        PRINT('Transforming Data for Table: silver.crm_prd_info');
+        WITH
+        TransformedData
         AS
         (
             SELECT
@@ -98,43 +122,42 @@ BEGIN
                 CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt
             FROM bronze.crm_prd_info
         )
-        INSERT INTO silver.crm_prd_info
-            (prd_id, cat_id, prd_key, prd_nm, prd_cost, prd_line, prd_start_dt, prd_end_dt)
-        SELECT
-            prd_id,
-            cat_id,
-            prd_key,
-            prd_nm,
-            prd_cost,
-            prd_line,
-            prd_start_dt,
-            prd_end_dt
-        FROM TransformedData;
+    INSERT INTO silver.crm_prd_info
+        (prd_id, cat_id, prd_key, prd_nm, prd_cost, prd_line, prd_start_dt, prd_end_dt)
+    SELECT
+        prd_id,
+        cat_id,
+        prd_key,
+        prd_nm,
+        prd_cost,
+        prd_line,
+        prd_start_dt,
+        prd_end_dt
+    FROM TransformedData;
 
         SET @end_time = GETDATE();
         PRINT('Time taken to load silver.crm_prd_info: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds');
         PRINT('----------------------------------------------');
 
-        -- Transform and load sales details into silver.crm_sales_details
+        -- Transform and Load crm_sales_details
         SET @start_time = GETDATE();
-        PRINT('Transforming and loading data into silver.crm_sales_details');
-
+        PRINT('Transforming Data for Table: silver.crm_sales_details');
         INSERT INTO silver.crm_sales_details
-            (sls_ord_num, sls_prd_key, sls_cust_id, sls_order_dt, sls_ship_dt, sls_due_dt, sls_sales, sls_quantity, sls_price)
-        SELECT
-            sls_ord_num,
-            sls_prd_key,
-            sls_cust_id,
-            CASE 
+        (sls_ord_num, sls_prd_key, sls_cust_id, sls_order_dt, sls_ship_dt, sls_due_dt, sls_sales, sls_quantity, sls_price)
+    SELECT
+        sls_ord_num,
+        sls_prd_key,
+        sls_cust_id,
+        CASE 
                 WHEN sls_order_dt > '2050-01-01' OR sls_order_dt < '1900-01-01' THEN DATEADD(DAY, -1, sls_ship_dt)
                 ELSE sls_order_dt
             END AS sls_order_dt,
-            CASE 
+        CASE 
                 WHEN sls_ship_dt > sls_due_dt THEN DATEADD(DAY, -1, sls_due_dt)
                 ELSE sls_ship_dt
             END AS sls_ship_dt,
-            sls_due_dt,
-            CASE 
+        sls_due_dt,
+        CASE 
                 WHEN sls_sales IS NULL OR sls_sales <= 0 THEN 
                     CASE 
                         WHEN sls_quantity IS NOT NULL AND sls_quantity > 0 AND sls_price IS NOT NULL AND sls_price > 0 THEN sls_quantity * sls_price
@@ -142,7 +165,7 @@ BEGIN
                     END
                 ELSE sls_sales
             END AS sls_sales,
-            CASE 
+        CASE 
                 WHEN sls_quantity IS NULL OR sls_quantity <= 0 THEN 
                     CASE 
                         WHEN sls_sales IS NOT NULL AND sls_sales > 0 AND sls_price IS NOT NULL AND sls_price > 0 THEN sls_sales / sls_price
@@ -150,7 +173,7 @@ BEGIN
                     END
                 ELSE sls_quantity
             END AS sls_quantity,
-            CASE 
+        CASE 
                 WHEN sls_price IS NULL OR sls_price <= 0 THEN 
                     CASE 
                         WHEN sls_sales IS NOT NULL AND sls_sales > 0 AND sls_quantity IS NOT NULL AND sls_quantity > 0 THEN sls_sales / sls_quantity
@@ -158,52 +181,50 @@ BEGIN
                     END
                 ELSE sls_price
             END AS sls_price
-        FROM bronze.crm_sales_details;
+    FROM bronze.crm_sales_details;
 
         SET @end_time = GETDATE();
         PRINT('Time taken to load silver.crm_sales_details: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds');
         PRINT('----------------------------------------------');
 
         PRINT('--------------------------------------------------');
-        PRINT('Loading ERP Tables...');
+        PRINT('Transforming and Loading ERP Tables...');
         PRINT('--------------------------------------------------');
 
-        -- Transform and load customer information into silver.erp_cust_az12
+        -- Transform and Load erp_cust_az12
         SET @start_time = GETDATE();
-        PRINT('Transforming and loading data into silver.erp_cust_az12');
-
+        PRINT('Transforming Data for Table: silver.erp_cust_az12');
         INSERT INTO silver.erp_cust_az12
-            (cid, bdate, gen)
-        SELECT
-            CASE 
+        (cid, bdate, gen)
+    SELECT
+        CASE 
                 WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) 
                 WHEN cid LIKE 'AWO%' THEN SUBSTRING(cid, 4, LEN(cid)) 
                 ELSE cid 
             END AS cid,
-            CASE 
+        CASE 
                 WHEN bdate > GETDATE() THEN NULL 
                 ELSE bdate 
             END AS bdate,
-            CASE 
+        CASE 
                 WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
                 WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
                 ELSE 'n/a'
             END AS gen
-        FROM bronze.erp_cust_az12;
+    FROM bronze.erp_cust_az12;
 
         SET @end_time = GETDATE();
         PRINT('Time taken to load silver.erp_cust_az12: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds');
         PRINT('----------------------------------------------');
 
-        -- Transform and load location information into silver.erp_loc_a101
+        -- Transform and Load erp_loc_a101
         SET @start_time = GETDATE();
-        PRINT('Transforming and loading data into silver.erp_loc_a101');
-
+        PRINT('Transforming Data for Table: silver.erp_loc_a101');
         INSERT INTO silver.erp_loc_a101
-            (cid, cntry)
-        SELECT
-            REPLACE(cid, '-', '') AS cid,
-            CASE 
+        (cid, cntry)
+    SELECT
+        REPLACE(cid, '-', '') AS cid,
+        CASE 
                 WHEN UPPER(TRIM(cntry)) IN ('DE', 'GERMANY') THEN 'Germany'
                 WHEN UPPER(TRIM(cntry)) IN ('USA', 'UNITED STATES', 'US') THEN 'United States'
                 WHEN UPPER(TRIM(cntry)) = 'AUSTRALIA' THEN 'Australia'
@@ -212,30 +233,29 @@ BEGIN
                 WHEN UPPER(TRIM(cntry)) = 'FRANCE' THEN 'France'
                 ELSE 'n/a'
             END AS cntry
-        FROM bronze.erp_loc_a101;
+    FROM bronze.erp_loc_a101;
 
         SET @end_time = GETDATE();
         PRINT('Time taken to load silver.erp_loc_a101: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds');
         PRINT('----------------------------------------------');
 
-        -- Load product category information into silver.erp_px_cat_g1v2
+        -- Transform and Load erp_px_cat_g1v2
         SET @start_time = GETDATE();
-        PRINT('Loading data into silver.erp_px_cat_g1v2');
-
-        INSERT INTO silver.erp_px_cat_g1v2 (id, cat, subcat, maintenace)
-        SELECT 
-            id,
-            cat,
-            subcat,
-            maintenace
-        FROM bronze.erp_px_cat_g1v2;
+        PRINT('Transforming Data for Table: silver.erp_px_cat_g1v2');
+        INSERT INTO silver.erp_px_cat_g1v2
+        (id, cat, subcat, maintenace)
+    SELECT
+        id,
+        cat,
+        subcat,
+        maintenace
+    FROM bronze.erp_px_cat_g1v2;
 
         SET @end_time = GETDATE();
         PRINT('Time taken to load silver.erp_px_cat_g1v2: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds');
         PRINT('----------------------------------------------');
 
         SET @batch_end_time = GETDATE();
-
         PRINT('Time taken to load all tables: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds');
         PRINT('==================================================');
         PRINT('Data loaded successfully into silver layer...');
@@ -250,4 +270,4 @@ BEGIN
         PRINT('An error occurred: ' + ERROR_MESSAGE());
         PRINT('==================================================');
     END CATCH
-END
+END;
