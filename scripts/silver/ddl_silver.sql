bdc2245a919f7f96da1060fa79590041d39ee758
TheNisham <77533996+nishamghimire5@users.noreply.github.com>
2025-03-06 23:53:37 +0545

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -108,7 +108,7 @@ CREATE TABLE silver.erp_px_cat_g1v2
     id NVARCHAR(50),
     cat NVARCHAR(50),
     subcat NVARCHAR(50),
-    maintenace NVARCHAR(50),
+    maintenance NVARCHAR(50),
     dwh_create_date DATETIME DEFAULT GetDate()
 );
 GO
